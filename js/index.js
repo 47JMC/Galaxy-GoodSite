f590376ece8ee509f1a6ceefeaa7b483c1a0b250
47JMC <mjabir.5b@gmail.com>
2024-08-04 16:02:07 +0000

    Updated CSS and JS for cleaner github info

--- js/index.js
+++ js/index.js
@@ -1,98 +1,123 @@
 // When the page loads, check if the user has a saved theme
-let Savedtheme = localStorage.getItem("theme") || "light_mode"
-let element = document.getElementById("theme-changer-span")
+let Savedtheme = localStorage.getItem("theme") || "light_mode";
+let element = document.getElementById("theme-changer-span");
 if (Savedtheme === "dark_mode") {
-    document.body.classList.add("dark-mode")
-    element.innerText = "dark_mode"
-} else { element.innerText = "light_mode" }
+  document.body.classList.add("dark-mode");
+  element.innerText = "dark_mode";
+} else {
+  element.innerText = "light_mode";
+}
 
 function ChangeTheme() {
-    let theme = document.body.classList.toggle("dark-mode")
-    if (theme) {
-        localStorage.setItem("theme", "dark_mode")
-    }
-    else {
-        localStorage.setItem("theme", "light_mode")
-    }
-    element.innerText = localStorage.getItem("theme")
+  let theme = document.body.classList.toggle("dark-mode");
+  if (theme) {
+    localStorage.setItem("theme", "dark_mode");
+  } else {
+    localStorage.setItem("theme", "light_mode");
+  }
+  element.innerText = localStorage.getItem("theme");
 }
 
 function PhoneNavbarOptionsToggler() {
-    let phoneOptions = document.getElementById("phone-options")
-    if (phoneOptions.style.display === "none") {
-        phoneOptions.style.display = "block"
+  let phoneOptions = document.getElementById("phone-options");
+  if (phoneOptions.style.display === "none") {
+    phoneOptions.style.display = "block";
 
-        let profilePicture = document.getElementById("github-profile-picture")
-        let githubDisplayName = document.getElementById("github-display-name")
-        let githubBio = document.getElementById("github-bio")
-        let github_PublicRepos = document.getElementById("github-public_repos")
-        let github_Company = document.getElementById("github-company")
-        let github_Email = document.getElementById("github-email")
-        let github_Location = document.getElementById("github-location")
-        let github_hireable = document.getElementById("github-hireable")
-        let github_followers = document.getElementById("github-followers")
-        let github_Following = document.getElementById("github-following")
+    let profilePicture = document.getElementById("github-profile-picture");
+    let githubDisplayName = document.getElementById("github-display-name");
+    let githubBio = document.getElementById("github-bio");
+    let github_PublicRepos = document.getElementById("github-public_repos");
+    let github_Company = document.getElementById("github-company");
+    let github_Email = document.getElementById("github-email");
+    let github_Location = document.getElementById("github-location");
+    let github_hireable = document.getElementById("github-hireable");
+    let github_followers = document.getElementById("github-followers");
+    let github_Following = document.getElementById("github-following");
 
-        githubDisplayName.style.display = "none"
-        githubBio.style.display = "none"
-        github_PublicRepos.style.display = "none"
-        github_Company.style.display = "none"
-        github_Email.style.display = "none"
-        github_Location.style.display = "none"
-        github_hireable.style.display = "none"
-        github_followers.style.display = "none"
-        github_Following.style.display = "none"
-        profilePicture.style.display = "none"
-    }
-    else {
-        phoneOptions.style.display = "none"
-    }
+    githubDisplayName.style.display = "none";
+    githubBio.style.display = "none";
+    github_PublicRepos.style.display = "none";
+    github_Company.style.display = "none";
+    github_Email.style.display = "none";
+    github_Location.style.display = "none";
+    github_hireable.style.display = "none";
+    github_followers.style.display = "none";
+    github_Following.style.display = "none";
+    profilePicture.style.display = "none";
+  } else {
+    phoneOptions.style.display = "none";
+  }
 }
 
 async function GithubInfo() {
-    let githubDisplayName = document.getElementById("github-display-name")
-    try {
-        let github_username = document.getElementById("github-username").value;
-        let response = await fetch(`https://api.github.com/users/${github_username}`);
-        let data = await response.json();
+  let githubDisplayName = document.getElementById("github-display-name");
+  try {
+    let github_username = document.getElementById("github-username").value;
+    let response = await fetch(
+      `https://api.github.com/users/${github_username}`
+    );
+    let data = await response.json();
 
-        let profilePicture = document.getElementById("github-profile-picture")
-        let githubBio = document.getElementById("github-bio")
-        let github_PublicRepos = document.getElementById("github-public_repos")
-        let github_Company = document.getElementById("github-company")
-        let github_Email = document.getElementById("github-email")
-        let github_Location = document.getElementById("github-location")
-        let github_hireable = document.getElementById("github-hireable")
-        let github_followers = document.getElementById("github-followers")
-        let github_Following = document.getElementById("github-following")
+    let profilePicture = document.getElementById("github-profile-picture");
+    let githubBio = document.getElementById("github-bio");
+    let github_PublicRepos = document.getElementById("github-public_repos");
+    let github_Company = document.getElementById("github-company");
+    let github_Email = document.getElementById("github-email");
+    let github_Location = document.getElementById("github-location");
+    let github_hireable = document.getElementById("github-hireable");
+    let github_followers = document.getElementById("github-followers");
+    let github_Following = document.getElementById("github-following");
 
-        githubDisplayName.style.display = "block"
-        githubBio.style.display = "block"
-        github_PublicRepos.style.display = "block"
-        github_Company.style.display = "block"
-        github_Email.style.display = "block"
-        github_Location.style.display = "block"
-        github_hireable.style.display = "block"
-        github_followers.style.display = "block"
-        github_Following.style.display = "block"
+    githubDisplayName.style.display = "block";
+    githubBio.style.display = "block";
+    github_PublicRepos.style.display = "block";
+    github_Company.style.display = "block";
+    github_Email.style.display = "block";
+    github_Location.style.display = "block";
+    github_hireable.style.display = "block";
+    github_followers.style.display = "block";
+    github_Following.style.display = "block";
 
-        githubDisplayName.innerText = data.name || "No Display Name";
-        githubBio.innerText = data.bio || "No description";
-        github_PublicRepos.innerText = `Public Repositories: ${data.public_repos}`;
-        github_Company.innerText = "Company: " + data.company || "No Company";
-        github_Email.innerText = "Email: " + data.email || "No Email";
-        github_Location.innerText = "Location: " + data.location
-        github_hireable.innerText = "Hireable: " + data.hireable
-        github_followers.innerText = `Followers: ${data.followers}`;
-        github_Following.innerText = `Following: ${data.following}`;
+    profilePicture.classList.add("animate");
+    githubDisplayName.classList.add("animate");
+    githubBio.classList.add("animate");
+    github_PublicRepos.classList.add("animate");
+    github_Company.classList.add("animate");
+    github_Email.classList.add("animate");
+    github_Location.classList.add("animate");
+    github_hireable.classList.add("animate");
+    github_followers.classList.add("animate");
+    github_Following.classList.add("animate");
 
-        if (data.avatar_url) {
-            profilePicture.style.display = "block"
-            profilePicture.src = data.avatar_url
-        }
+    githubDisplayName.innerText = data.name || "No Display Name";
+    githubBio.innerText = data.bio || "No description";
+    github_PublicRepos.innerText = `Public Repositories: ${data.public_repos}`;
+    github_Company.innerText = `Company: ${data.company}` || "No Company";
+    github_Email.innerText = `Email: ${data.email}` || "No Email";
+    github_Location.innerText = `Location: ${data.location}` || "No Location";
+    github_hireable.innerText = `Hireable: ${data.hireable}` || "Not hireable";
+    github_followers.innerText = `Followers: ${data.followers}`;
+    github_Following.innerText = `Following: ${data.following}`;
+
+    setTimeout(() => {
+      profilePicture.classList.remove("animate");
+      githubDisplayName.classList.remove("animate");
+      githubBio.classList.remove("animate");
+      github_PublicRepos.classList.remove("animate");
+      github_Company.classList.remove("animate");
+      github_Email.classList.remove("animate");
+      github_Location.classList.remove("animate");
+      github_hireable.classList.remove("animate");
+      github_followers.classList.remove("animate");
+      github_Following.classList.remove("animate");
+    }, 2000);
+
+    if (data.avatar_url) {
+      profilePicture.style.display = "block";
+      profilePicture.src = data.avatar_url;
     }
-    catch (error) {
-        githubDisplayName.innerText = "Error: " + error.message
-        githubDisplayName.style.display = "block"
-    }
+  } catch (error) {
+    githubDisplayName.innerText = "Error: " + error.message;
+    githubDisplayName.style.display = "block";
+  }
 }
